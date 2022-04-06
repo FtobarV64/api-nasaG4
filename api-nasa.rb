232323f54a5131c7402eb6dda5edaeb38c8731f3
FtobarV64 <ftobarv@gmail.com>
2022-04-06 18:59:04 -0400

    response 1 finished commit

--- api-nasa.rb
+++ api-nasa.rb
@@ -28,3 +28,20 @@
 # Se revisará la estructura del documento generado.
 # 3. Pregunta bonus: Crear un método photos_count que reciba el hash de respuesta y
 # devuelva un nuevo hash con el nombre de la cámara y la cantidad de fotos.
+
+require "uri"
+require "net/http"
+require "json"
+
+# 1. Crear el método request que reciba una url y el api_key y devuelva el hash con los
+# resultados.
+def request(url, api_key)
+    url = URI(url+api_key)
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+    request = Net::HTTP::Get.new(url)
+    response = https.request(request)
+    response = JSON.parse(response.read_body)
+end
+# Prueba del método con dos argumentos 
+data = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=","ZXliFADZo3aNDlJSHxU6P2vf37CHG5ZdED1HPwmZ")
